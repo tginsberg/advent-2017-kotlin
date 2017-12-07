258878bfef7c6d30dffb34991248e266a59e61d0
Todd Ginsberg <todd@ginsberg.com>
2017-12-07 14:23:55 -0600

    Day 7 Optimizations
    + Create regex once.
    + Lazy properties, calculate once.
    + Calculate imbalance once.

--- src/main/kotlin/com/ginsberg/advent2017/Day07.kt
+++ src/main/kotlin/com/ginsberg/advent2017/Day07.kt
@@ -24,14 +24,15 @@ class Day07(input: List<String>) {
     private fun parseInput(input: List<String>): Node {
         val nodesByName = mutableMapOf<String, Node>()
         val parentChildPairs = mutableSetOf<Pair<Node, String>>()
+        val rowRegex = """\w+""".toRegex()
 
         input.forEach {
-            val groups = """\w+""".toRegex().findAll(it).toList().map { it.value }
-            val node = Node(groups[0].trim(), groups[1].toInt())
+            val groups = rowRegex.findAll(it).toList().map { it.value }
+            val node = Node(groups[0], groups[1].toInt())
             nodesByName.put(node.name, node)
 
             groups.drop(2).forEach {
-                parentChildPairs.add(Pair(node, it.trim()))
+                parentChildPairs.add(Pair(node, it))
             }
         }
 
@@ -56,27 +57,28 @@ data class Node(val name: String,
 
     fun findImbalance(imbalance: Int = 0): Int =
 
-        if (imbalance > 0 && childrenAreBalanced()) {
+        if (imbalance > 0 && childrenAreBalanced) {
             // We end when I have a positive imbalance and my children are balanced.
             weight - imbalance
         } else {
             // Find the child tree that is off.
-            val subtreesByWeight = children.groupBy { it.totalWeight() }
+            val subtreesByWeight = children.groupBy { it.totalWeight }
 
             // Find the imbalanced child tree (they will be the lone node in the list, when grouped by weight)
             val badTree = subtreesByWeight.minBy { it.value.size }?.value?.first()
                 ?: throw IllegalStateException("Should not be balanced here.")
 
+            // Recurse, passing down our imbalance. If we don't know the imbalance, calculate it.
             // Calculate the imbalance as the absolute value of the difference of all distinct weights
-            val absBadWeight = subtreesByWeight.keys.reduce { a, b -> a - b }.absoluteValue
-
-            badTree.findImbalance(absBadWeight)
+            badTree.findImbalance(imbalance.takeIf { it != 0 } ?: subtreesByWeight.keys.reduce { a, b -> a - b }.absoluteValue)
         }
 
-    private fun totalWeight(): Int =
-        weight + children.sumBy { it.totalWeight() }
+    private val totalWeight: Int by lazy {
+        weight + children.sumBy { it.totalWeight }
+    }
 
-    private fun childrenAreBalanced(): Boolean =
-        children.map { it.totalWeight() }.distinct().size == 1
+    private val childrenAreBalanced: Boolean by lazy {
+        children.map { it.totalWeight }.distinct().size == 1
+    }
 
 }
